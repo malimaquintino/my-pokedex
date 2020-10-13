d3c99d216216c505548cb3694659cefa9c2a8988
Matheus de Lima Quintino <malimaquintino@gmail.com>
2020-10-13 20:51:35 -0300

    filterbar

--- src/pages/pokedex/index.js
+++ src/pages/pokedex/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import datainfo from '../../data/datainfo.json'
 import Pokecard from '../../components/pokecard'
 import './style.css'
 
 function Pokedex (){
+    const [search, setSearch] = useState('')
 
     return (
-
         <>
             <h1>Pokédex</h1>
-            <div className="container">
-                { datainfo.map((item, i)=> <Pokecard key={i}  {...item}/>)}
+            <div>
+                <input type="text" onChange={(e) => setSearch(e.target.value)} />
+            </div>
+            <div className="main-container">
+                { datainfo.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase())).map((filteredPokemon,i) => (<Pokecard key={i}  {...filteredPokemon}/>) ) }                
             </div>
             
         </>
