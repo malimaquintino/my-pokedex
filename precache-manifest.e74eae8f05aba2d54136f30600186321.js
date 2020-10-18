a644e2154e8e5176942472017aa45f0cc9fddf51
Matheus de Lima Quintino <malimaquintino@gmail.com>
2020-10-18 17:03:04 -0300

    Updates

---
+++ precache-manifest.e74eae8f05aba2d54136f30600186321.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f093825211bdfcfcf1daf24f29f44fd8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2cbf114627d3e094c556",
+    "url": "./static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "2b4a936dc88ba570bf4d",
+    "url": "./static/css/main.104c1e4c.chunk.css"
+  },
+  {
+    "revision": "2cbf114627d3e094c556",
+    "url": "./static/js/2.ca51051a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ca51051a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4a936dc88ba570bf4d",
+    "url": "./static/js/main.4f8c6de0.chunk.js"
+  },
+  {
+    "revision": "3f49aec4ebdd2bc1317f",
+    "url": "./static/js/runtime-main.2267497a.js"
+  },
+  {
+    "revision": "9ba4d684e64c19af7c99148ac36af46b",
+    "url": "./static/media/001.9ba4d684.png"
+  },
+  {
+    "revision": "28cc8e884bbbf08ac3b44b4013a3d5db",
+    "url": "./static/media/002.28cc8e88.png"
+  },
+  {
+    "revision": "8c37e2ae54a20df159b6706ef5663ff9",
+    "url": "./static/media/003.8c37e2ae.png"
+  },
+  {
+    "revision": "9be8f2a81b006e2f8897f1889107d0f7",
+    "url": "./static/media/004.9be8f2a8.png"
+  },
+  {
+    "revision": "4e83c7dbbb912257f06b13cc1468f76f",
+    "url": "./static/media/005.4e83c7db.png"
+  },
+  {
+    "revision": "2d32cf3c3042861de658c96052673408",
+    "url": "./static/media/006.2d32cf3c.png"
+  },
+  {
+    "revision": "7b9ddf692c6df64d86bbe4e96ef46a83",
+    "url": "./static/media/007.7b9ddf69.png"
+  },
+  {
+    "revision": "04cfac8634923b57a079cc4381035686",
+    "url": "./static/media/008.04cfac86.png"
+  },
+  {
+    "revision": "14aba60b06ced6ea846ad63b6611f524",
+    "url": "./static/media/009.14aba60b.png"
+  },
+  {
+    "revision": "799de73ed9a4d32ad6c08e7dc8fcfc50",
+    "url": "./static/media/010.799de73e.png"
+  },
+  {
+    "revision": "5bc90ea068bdd1162f0bf10a64326fe2",
+    "url": "./static/media/011.5bc90ea0.png"
+  },
+  {
+    "revision": "eb6566a45da0d7770ac2e01025b657bd",
+    "url": "./static/media/012.eb6566a4.png"
+  },
+  {
+    "revision": "a36e71da6ec97bf184a71883476152b0",
+    "url": "./static/media/013.a36e71da.png"
+  },
+  {
+    "revision": "06d3055f204c544a79f3ce6e9103a4dc",
+    "url": "./static/media/014.06d3055f.png"
+  },
+  {
+    "revision": "7fc54db4c58e2e51f4999ce66c12448a",
+    "url": "./static/media/015.7fc54db4.png"
+  },
+  {
+    "revision": "15ca3e1cc06bcdbc380662f2432008ee",
+    "url": "./static/media/016.15ca3e1c.png"
+  },
+  {
+    "revision": "f1972cf117fbc7544f798e0fec399414",
+    "url": "./static/media/017.f1972cf1.png"
+  },
+  {
+    "revision": "b33fccccc3c38657fbf66bda12a7ca90",
+    "url": "./static/media/018.b33fcccc.png"
+  },
+  {
+    "revision": "d21d04d8488e11ee9a04a9d8b3e6573a",
+    "url": "./static/media/019.d21d04d8.png"
+  },
+  {
+    "revision": "5a34e7eeebc70b99bd4d5f7694edb452",
+    "url": "./static/media/020.5a34e7ee.png"
+  },
+  {
+    "revision": "f993baa8707a5cc7638c8434b3d8f9e4",
+    "url": "./static/media/021.f993baa8.png"
+  },
+  {
+    "revision": "2640107a291d1d0b1700a42273c467f8",
+    "url": "./static/media/022.2640107a.png"
+  },
+  {
+    "revision": "bd66b4f62115f19d42302758677525b8",
+    "url": "./static/media/023.bd66b4f6.png"
+  },
+  {
+    "revision": "44ab0aecdfa80c2df6060ff82a6d067b",
+    "url": "./static/media/024.44ab0aec.png"
+  },
+  {
+    "revision": "53dbfd304766b5858ee6e139646713c7",
+    "url": "./static/media/025.53dbfd30.png"
+  },
+  {
+    "revision": "f3e7b764831b656de7bdb1be1445e766",
+    "url": "./static/media/026.f3e7b764.png"
+  },
+  {
+    "revision": "2c4cebbd67505ea15634b3a81a291b1c",
+    "url": "./static/media/027.2c4cebbd.png"
+  },
+  {
+    "revision": "e4e761082a8714e94d3f077f68789712",
+    "url": "./static/media/028.e4e76108.png"
+  },
+  {
+    "revision": "52f72fbcca315b01a0a8ad447a45b719",
+    "url": "./static/media/029.52f72fbc.png"
+  },
+  {
+    "revision": "9445ac8fb36bd9ceea8359d9fe1f2a50",
+    "url": "./static/media/030.9445ac8f.png"
+  }
+]);
