de307039fd433481ea90fe68d9602975034f9d1b
Matheus de Lima Quintino <malimaquintino@gmail.com>
2020-10-18 17:20:49 -0300

    build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Pokedex from './pages/pokedex/';
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Route exact path='/' component={Pokedex} />
     </Router>
   );
